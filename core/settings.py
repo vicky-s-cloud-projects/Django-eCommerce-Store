efc3f249e8f87873727f22cad41c124ddebc6aa9
vivekchalla246 <vivekchalla246@gmail.com>
2025-12-13 20:18:59 +0000

    Fix PostgreSQL schema permissions for Django migrations

--- core/settings.py
+++ core/settings.py
@@ -99,12 +99,24 @@ AUTH_USER_MODEL = 'accounts.Account'
 # }
 
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
